782a82991c354eca97b17a6872093733ac4dbc53
JJTech0130 <james@erinpryorgill.com>
2020-07-09 17:37:44 -0400

    First test

--- generate.sh
+++ generate.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 ##FIXME: Versions are static
+git clone https://github.com/LWJGL/lwjgl.git
 versions=('master' 'lwjgl2.9.3' 'lwjgl2.9.2' 'lwjgl2.9.1' 'lwjgl2.9.0')
 for version in "${versions[@]}"
 do
   echo "Compiling LWJGL version $version."
+  cd lwjgl
+  git checkout $version
+  #git status
+  env PATH=${JAVA_HOME}/bin:${PATH} ant
+  cd ..
+  mkdir $version
+  cp lwjgl/libs/linux/* $version 
 done
