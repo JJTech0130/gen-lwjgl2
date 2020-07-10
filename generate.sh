759d87fdf9230adecf9cffc6b104467eccdab1e2
JJTech0130 <james@erinpryorgill.com>
2020-07-09 20:05:01 -0400

    Updated and fixed bugs

--- generate.sh
+++ generate.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
-##FIXME: Versions are static
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64 #Replace with java home, MUST BE OPENJDK 8
+
 git clone https://github.com/LWJGL/lwjgl.git
-versions=('master' 'lwjgl2.9.3' 'lwjgl2.9.2' 'lwjgl2.9.1' 'lwjgl2.9.0')
+
+#versions=('master' 'lwjgl2.9.3' 'lwjgl2.9.2' 'lwjgl2.9.1' 'lwjgl2.9.0') #Version 2.9.1 and 2.9.0 won't compile
+versions=('master' 'lwjgl2.9.3' 'lwjgl2.9.2') #Versions to compile, correspond to branch/tag names
+
 for version in "${versions[@]}"
 do
-  echo " ================ Compiling LWJGL version $version. ================ "
+  echo " $(tput setaf 6)================ Compiling LWJGL version $(tput bold)$version$(tput sgr 0)$(tput setaf 6). ================$(tput sgr 0) "
   echo " "
   cd lwjgl
   git reset --hard
   git checkout $version
-  git status
+  #git status
   echo "Using aarch64 version of JAWT"
   #Replace i386 with aarch64 in platform_build/linux_ant/build.xml
   sed -i 's/i386/aarch64/g' platform_build/linux_ant/build.xml
